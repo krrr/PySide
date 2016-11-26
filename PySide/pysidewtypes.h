0317473934048a27642f57f4dca154bbfd947783
krrr <guogaishiwo@gmail.com>
2016-11-26 15:22:42 +0800

    Fix QWidget::winEvent https://bugreports.qt.io/browse/PYSIDE-84

--- PySide/pysidewtypes.h
+++ PySide/pysidewtypes.h
@@ -1,12 +1,26 @@
 #ifndef __PYSIDEWTYPES__
 #define __PYSIDEWTYPES__
 
-typedef struct HWND__ *HWND;
-typedef unsigned UINT;
+// Windows types, used by QWidget::winEvent
+// see https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx
+#if defined(_WIN64)
+    typedef __int64 LONG_PTR;  // for MSVC compiler
+#else
+    typedef long LONG_PTR;
+#endif
+
+#if defined(_WIN64)
+    typedef unsigned __int64 UINT_PTR;
+#else
+    typedef unsigned int UINT_PTR;
+#endif
+
+typedef void * HWND;
+typedef unsigned int UINT;
 typedef long LONG;
 typedef unsigned long DWORD;
-typedef UINT WPARAM;
-typedef LONG LPARAM;
+typedef UINT_PTR WPARAM;
+typedef LONG_PTR LPARAM;
 
 struct POINT
 {
